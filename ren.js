0da0ca3acfb681617aca3ce09432fb5f275b36ba
vexen-cmd <harshsharmaff56@gmail.com>
2025-09-27 00:27:51 +0530

    fixed pause button

--- ren.js
+++ ren.js
@@ -1,38 +1,60 @@
 
-document.querySelector('.js-button').addEventListener('click', () => {
-    play()
-});
+let timer;
+let pause = false;
+let minutes = 0
+let seconds = 0
 
-document.querySelector('.js-button-reset').addEventListener('click', () => {
-    clearInterval(zeta)
-});
-
-
- let zeta;
- let pause=false;
 function play() {
-    let beta = 0
-    let count = -1
-    
-     zeta = setInterval(() => {
-        count++
-        if (count >= 60) {
-            count = 0
-            beta++
-            if (beta >= 25) {
-                beta = 0
-            }
-        }
-        console.log(beta, count)
-    }, 1000);
-}
-// correction needed isse pause krna h timer ko kaise kru??
+
+  timer = setInterval(() => {
+    seconds++
+    if (seconds >= 60) {
+      seconds = 0
+      minutes++
+    }
+
+    if (minutes >= 25) {
+      minutes = 0
+      return clearInterval(timer)
+    }
+    let innerHtmlText
+   if (minutes == 24 && seconds == 59) {
+      innerHtmlText = `00:${minutes + 1}:00`
+    } else {
+      innerHtmlText = `00:${minutes}:${seconds}`
+    }
+    document.querySelector('.ren-clock').innerHTML = innerHtmlText;
+  }, 1000);
+};
+
+
+document.querySelector('.js-button').addEventListener('click', () => {
+  clearInterval(timer)
+  play()
+});
+
 
 document.querySelector('.js-button-pause').addEventListener('click', () => {
-  if(!pause){
-    play()
-  }else{
-    pause=false
-    clearInterval(zeta)
-  }
+  pauseFunction()
 });
+
+
+document.querySelector('.js-button-reset').addEventListener('click', () => {
+  minutes = 0
+  seconds = 0
+  clearInterval(timer)
+  let displayTimer = `  00:00:00`
+  document.querySelector('.ren-clock').innerHTML = displayTimer
+});
+
+//couldn't get this part will try tommorrow
+function pauseFunction() {
+  if (pause == true) {
+    console.log('shanti')
+  } else {
+    clearInterval(timer)
+    pause = false
+  }
+};
+
+
