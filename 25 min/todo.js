9a12a14e3bd395e81036c8be88e15caabd7f6707
vexen-cmd <harshsharmaff56@gmail.com>
2025-09-29 23:25:31 +0530

    fix your todo

---
+++ 25 min/todo.js
@@ -0,0 +1,19 @@
+const array=[]
+const innerELem=document.querySelector('.js-input')
+
+//fix needed 
+// document.querySelector('.js-add').addEventListener('click',()=>{
+//     array.push(innerELem.value)
+//     innerELem.value=''
+
+//     console.log(array)
+    
+
+//     array.forEach((value,i) => {
+//         let sumup='';
+//         let  html =`<p>${value}</p>`;
+       
+//         document.querySelector('.your-todo').innerHTML=sumup+=html
+
+//     });
+// })
