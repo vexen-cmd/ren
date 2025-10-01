262a7d8d5ffb81e709f7eb00a387ca5feb18f81e
vexen-cmd <harshsharmaff56@gmail.com>
2025-10-01 23:58:43 +0530

    todo done but shit happening

--- 25 min/todo.js
+++ 25 min/todo.js
@@ -1,19 +1,17 @@
 const array=[]
 const innerELem=document.querySelector('.js-input')
 
-//fix needed 
-// document.querySelector('.js-add').addEventListener('click',()=>{
-//     array.push(innerELem.value)
-//     innerELem.value=''
 
-//     console.log(array)
+document.querySelector('.js-add').addEventListener('click',()=>{
+    array.push(innerELem.value)
+    innerELem.value=''
+
+    console.log(array)
     
-
-//     array.forEach((value,i) => {
-//         let sumup='';
-//         let  html =`<p>${value}</p>`;
-       
-//         document.querySelector('.your-todo').innerHTML=sumup+=html
-
-//     });
-// })
+ let mytodo='';
+    array.forEach((value,i) => {
+       let theta=`<p> ${value} </p>`
+       mytodo+=theta
+    });
+    document.querySelector('.your-todo').innerHTML=mytodo;
+});
