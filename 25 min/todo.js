374e249e0880dcb73641ac9b0f0531154ff17e28
vexen-cmd <harshsharmaff56@gmail.com>
2025-10-02 01:07:30 +0530

    work on todo.js made a mess

--- 25 min/todo.js
+++ 25 min/todo.js
@@ -1,17 +1,22 @@
-const array=[]
-const innerELem=document.querySelector('.js-input')
+const array = []
+const innerELem = document.querySelector('.js-input')
 
 
-document.querySelector('.js-add').addEventListener('click',()=>{
+document.querySelector('.js-add').addEventListener('click', () => {
     array.push(innerELem.value)
-    innerELem.value=''
+    innerELem.value = ''
 
     console.log(array)
-    
- let mytodo='';
-    array.forEach((value,i) => {
-       let theta=`<p> ${value} </p>`
-       mytodo+=theta
+
+    let mytodo = '';
+    array.forEach((value, i) => {
+        let theta = `<p class="todo-p">
+         ${value} </p>
+       <div class="button-div">
+        <button class="buttons"> Done </button>
+         <button class="buttons"> Delete </button>
+       </div>`
+        mytodo += theta
     });
-    document.querySelector('.your-todo').innerHTML=mytodo;
+    document.querySelector('.your-todo').innerHTML = mytodo;
 });
