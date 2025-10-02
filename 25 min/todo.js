fd46d4ef6de6c5052b35b9dfb899a641ba94b001
vexen-cmd <harshsharmaff56@gmail.com>
2025-10-02 19:17:56 +0530

    todo del

--- 25 min/todo.js
+++ 25 min/todo.js
@@ -1,22 +1,42 @@
 const array = []
-const innerELem = document.querySelector('.js-input')
-
 
 document.querySelector('.js-add').addEventListener('click', () => {
-    array.push(innerELem.value)
-    innerELem.value = ''
-
-    console.log(array)
-
-    let mytodo = '';
-    array.forEach((value, i) => {
-        let theta = `<p class="todo-p">
-         ${value} </p>
-       <div class="button-div">
-        <button class="buttons"> Done </button>
-         <button class="buttons"> Delete </button>
-       </div>`
-        mytodo += theta
-    });
-    document.querySelector('.your-todo').innerHTML = mytodo;
+  const innerELem = document.querySelector('.js-input')
+  array.push(innerELem.value)
+  innerELem.value = ''
+  render()
 });
+
+function render() {
+  let mytodo = '';
+  array.forEach((value) => {
+
+    if (value === '') {
+      return;
+    }
+
+    let theta = `<p class="todo-p">
+        ${value} </p>
+      <div class="button-div">
+      <button class="js-done buttons"> Done </button>
+        <button class="js-delete buttons"> Delete </button>
+      </div>`
+
+    mytodo += theta
+  });
+
+  document.querySelector('.your-todo').innerHTML = mytodo;
+
+
+  document.querySelectorAll('.js-delete').forEach((button, i) => {
+    button.addEventListener('click', () => {
+      array.splice(i, 1)
+      render()
+    })
+  });
+};
+
+
+
+
+
